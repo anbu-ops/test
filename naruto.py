5c6674b0e0c0f16d94108a34f9aec9995ac3e67c
Anbu_blackops <banerjeeankit71@gmail.com>
2020-10-24 19:17:48 +0530

    will change later

---
+++ naruto.py
@@ -0,0 +1,20 @@
+tail=9
+if tail==1 :
+        print('shukaku')
+elif tail==2:
+        print('matatabi')
+elif tail==3:
+        print('isobu')
+elif tail==4:
+        print('songoku')
+elif tail==5:
+        print('kokuo')
+elif tail==6:
+        print('saiken')
+elif tail==7:
+        print('chomei')
+elif tail==8:
+        print('gyuki')
+elif tail==9:
+        print('kurama')
+
